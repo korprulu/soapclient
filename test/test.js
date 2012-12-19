f500364c85a1792adbd07840f11493296ce757e4
Kevin Chiu <korprulu9@gmail.com>
2012-12-20 04:01:31 +0800

    Rewrite complete

--- test/test.js
+++ test/test.js
@@ -2,14 +2,27 @@ var assert = require("assert");
 var soapclient = require("../src/soapclient");
 
 describe("soapclient", function(){
-    var params = new soapclient.SOAPClientRequest();
+    var req = new soapclient.SOAPClientRequest("hello");
     describe("#toXml()", function(){
-        var root = params.addRootElement("hello"),
-            xml = params.toXml();
+        var xml = req.toXml();
 
         it("should return xml string", function() {
             assert.equal("string", typeof xml, typeof xml);
             console.log(xml);
         });
+    });
+
+    describe("#invoke()", function() {
+        it("should return soap response", function(done) {
+            var method = "listAllProcesses";
+            var req = new soapclient.SOAPClientRequest(method);
+            soapclient.SOAPClient.invoke(
+                "http://140.136.155.2/ode/processes/ProcessManagement",
+                req,
+                function(data) {
+                    assert.equal("string", typeof data, data);
+                    done();
+                });
+        }); 
     });
 });
